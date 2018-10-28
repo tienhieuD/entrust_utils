9e6f5e09259bc51dceeee42276af4339b8c3ad80
Tiến Hiệu <duongnguyen1216@gmail.com>
2018-10-29 00:14:00 +0700

    [add] number to text vietnamese

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "entrust_utils",
+    'version': '0.1',
+    'author': 'Entrust SA',
+    'maintainer': 'Entrust SA',
+    'website': "http://www.odoo.com",
+    'license': 'LGPL-3',
+    'category': 'Uncategorized',
+    'sequence': 1000,
+    'description': """Utils""",
+    'summary': """Some utils""",
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'qweb': [],
+    # 'js': ['static/src/js/first_module.js',],
+    # 'css': ['static/src/css/web_example.css',],
+    # 'images': ['static/description/icon.png',],
+    'auto_install': False,
+    'application': False,
+    'installable': True,
+    # 'external_dependencies': {'python' : ['usb.core','serial','qrcode']}
+    # 'pre_init_hook': 'pre_init_hook',
+    # 'post_init_hook': 'post_init_hook',
+    # 'uninstall_hook': 'uninstall_hook',
+}
---
+++ tools/number2text.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+
+def trim_recursion(string):
+    string = string.replace(SPACE_CHAR * 2, SPACE_CHAR)
+    return trim_recursion(string) if SPACE_CHAR * 2 in string else string
+
+
+SPACE_CHAR, CURRENCY, HUNDRED_UNIT = ' ', 'đồng', ['', 'mươi', 'trăm']
+UNIT_NAME = ['', 'nghìn', 'triệu', 'tỷ', 'nghìn tỷ', 'triệu tỷ', 'tỷ tỷ']
+NUMBER_TO_STRING = {'0': 'không', '1': 'một', '2': 'hai', '3': 'ba', '4': 'bốn', '5': 'năm', '6': 'sáu', '7': 'bảy', '8': 'tám', '9': 'chín'}
+REPLACE_WORD = {'không mươi': 'linh', 'linh không': '', 'mươi không': 'mươi', 'một mươi': 'mười', 'mươi bốn': 'mươi tư', 'mười năm': 'mười lăm', 'mươi một': 'mươi mốt', 'mươi năm': 'mươi lăm', }
+REPLACE_WORD_2 = dict([(trim_recursion(SPACE_CHAR.join((NUMBER_TO_STRING['0'], HUNDRED_UNIT[2], unit_name, CURRENCY))), CURRENCY) for unit_name in UNIT_NAME])
+
+
+def number2text_vn(n):
+    """ Convert number to Vietnam currency """
+    float_number = float(n) if not isinstance(n, float) else n
+    integer_part = int(float_number)
+    decimal_part = str(float_number).split('.')[1] if '.' in str(float_number) else 0
+
+    def read_hundred(hundred_number_s='025'):
+        res, index = '', 0
+        for char in reversed(hundred_number_s):
+            res = SPACE_CHAR.join((NUMBER_TO_STRING[char], HUNDRED_UNIT[index], res))
+            index += 1
+        return res
+
+    def read_number(number_s='1600300444'):
+        groups = [number_s[::-1][i:i + 3][::-1] for i in range(0, len(number_s), 3)]
+        res, index = '', 0
+        for hundred in groups:
+            res = SPACE_CHAR.join((read_hundred(hundred), UNIT_NAME[index], res))
+            index += 1
+        return res
+
+    def to_string(money):
+        string_money = str(money) if not isinstance(money, str) else money
+        string_money = read_number(string_money)
+        return string_money
+
+    string_integer = to_string(integer_part)
+    string_decimal = to_string(decimal_part) if int(decimal_part) else ''
+    comma = 'phẩy' if string_decimal else ''
+    result = trim_recursion(SPACE_CHAR.join((string_integer, comma, string_decimal, CURRENCY)))
+
+    # Replace wrong words
+    for wrong_word, right_word in REPLACE_WORD.items():
+        result = trim_recursion(result.replace(wrong_word, right_word))
+    for wrong_word, right_word in REPLACE_WORD_2.items():
+        result = trim_recursion(result.replace(trim_recursion(wrong_word), right_word))
+
+    return result.strip()
